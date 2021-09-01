b5b556361b212d6b52d3276cbf54039f3d38918d
SarkerNayon <87502361+SarkerNayon@users.noreply.github.com>
2021-09-01 18:16:42 -0400

    09012021 6:16PM

---
+++ src/xPath_project/HelthCare.java
@@ -0,0 +1,17 @@
+package xPath_project;
+
+public class HelthCare {
+
+	public static void main(String[] args) {
+		/*
+		 * We are trying to find as many as qni
+		 */
+		
+		/*
+		 * Ex-1 : 
+		 * Source Feild :- United Healthcare (uhc.com)
+		 * Search Button :-//input[@class='search-input borderRadius-35 m-0 py-2 px-3 weight-medium desktop']
+		 * 
+		 */
+		
+}
